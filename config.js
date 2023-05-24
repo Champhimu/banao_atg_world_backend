1090241fe958dfa1962475a7be6490d704faa1ab
Himanshu Thakur <champhimu359@gmail.com>
2023-05-24 19:42:38 +0530

    config updated

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+    JWT_SECRET : "TXNQzh7dckQ/Kb7w0w2TaYI/cHd7y7VRezalHeWOQs=",
+    // EMAIL: "eloisa26@ethereal.email",
+    // PASSWORD: "uHZk4rtdj2bEmkCmre",
+    EMAIL: 'joaquin.rowe@ethereal.email',
+    PASSWORD: 'qJ2HTfAgCgmMwRR34v',
+    // EMAIL: "champhimu359@gmail.com",
+    // PASSWORD: "ytidwobuftnanuvy",
+    ATLAS_URI: "mongodb+srv://typrojectmanagement:%23Himanshu%40123@cluster0.mjino1h.mongodb.net/"
+}
