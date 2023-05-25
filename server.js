141c4c9411f41d32ffb149a6316d3a978e3d9761
Himanshu Thakur <champhimu359@gmail.com>
2023-05-25 11:03:56 +0530

    bodyparser added

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ import morgan from 'morgan';
 import connect from './database/conn.js'; 
 import router from './router/route.js';
 import post from './router/post.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+// app.use(express.json({ limit: "100mb" }));
+// app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); //less hackers know about our stack
+
 
 const port = 8080;
 
@@ -28,13 +36,6 @@ app.get("/",(req,res) => {
 app.use('/api', router)
 app.use('/api',post)
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-});
 
 // start server only when we have valid connection
 connect().then(() => {
